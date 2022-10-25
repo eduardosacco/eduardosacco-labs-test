34f5f020914b33fdcb3bc30ce84248001d3ec138
Eduardo Sacco <saccoeduardo@gmail.com>
2022-10-25 19:19:41 -0300

    Add convert API and tests

---
+++ pages/api/convert.spec.ts
@@ -0,0 +1,86 @@
+import { getChunksOfThree, convertToWords } from './convert';
+
+describe('getChunksOfThree', () => {
+  it.each([
+    ['1', ['001']],
+    ['12', ['012']],
+    ['123', ['123']],
+    ['1234', ['001', '234']],
+    ['12345', ['012', '345']],
+    ['123456', ['123', '456']],
+    ['1234567', ['001', '234', '567']],
+    ['0123456789', ['000', '123', '456', '789']],
+  ])('should correctly separate %s into chunks of three', (input, expected) => {
+    const chuncks = getChunksOfThree(input);
+    expect(chuncks).toEqual(expected);
+  });
+});
+
+describe('convertToWords', () => {
+  const MAX_ALLOWED_NUMBER = 9999999999999;
+  const ERROR_MAX_ALLOWED_NUMBER = `Maximum allowed number is ${MAX_ALLOWED_NUMBER}`;
+  const ERROR_NOT_POSITIVE_INTEGER = 'Number must be a positive integer';
+
+  it.each([
+    ['-1', ERROR_NOT_POSITIVE_INTEGER],
+    ['sdadas', ERROR_NOT_POSITIVE_INTEGER],
+    ['123.32', ERROR_NOT_POSITIVE_INTEGER],
+    ['123,32', ERROR_NOT_POSITIVE_INTEGER],
+    ['123,320.2', ERROR_NOT_POSITIVE_INTEGER],
+    [`${(+MAX_ALLOWED_NUMBER + 1).toString()}`, ERROR_MAX_ALLOWED_NUMBER],
+  ])(
+    'should return error when called with invalid input: %s',
+    (input, expected) => {
+      const result = convertToWords(input);
+      expect(result.isSuccess).toBeFalsy();
+      expect(result.data).toEqual(expected);
+    }
+  );
+
+  it.each([
+    ['0', 'zero'],
+    ['3', 'three'],
+    ['10', 'ten'],
+    ['13', 'thirteen'],
+    ['19', 'nineteen'],
+    ['20', 'twenty'],
+    ['33', 'thirty-three'],
+    ['41', 'forty-one'],
+    ['99', 'ninety-nine'],
+    ['100', 'one hundred'],
+    ['111', 'one hundred eleven'],
+    ['157', 'one hundred fifty-seven'],
+    ['1789', 'one thousand seven hundred eighty-nine'],
+    ['9999', 'nine thousand nine hundred ninety-nine'],
+    ['99999', 'ninety-nine thousand nine hundred ninety-nine'],
+    ['999999', 'nine hundred ninety-nine thousand nine hundred ninety-nine'],
+    [
+      '9999999',
+      'nine million nine hundred ninety-nine thousand nine hundred ninety-nine',
+    ],
+    [
+      '49823569',
+      'forty-nine million eight hundred twenty-three thousand five hundred sixty-nine',
+    ],
+    [
+      '123456789',
+      'one hundred twenty-three million four hundred fifty-six thousand seven hundred eighty-nine',
+    ],
+    [
+      '9999999999',
+      'nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine',
+    ],
+    [
+      '37834267593',
+      'thirty-seven billion eight hundred thirty-four million two hundred sixty-seven thousand five hundred ninety-three',
+    ],
+    [
+      '9999999999999',
+      'nine trillion nine hundred ninety-nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine',
+    ],
+  ])('should correctly convert %s into words', (input, expected) => {
+    const result = convertToWords(input);
+    expect(result.isSuccess).toBeTruthy();
+    expect(result.data).toEqual(expected);
+  });
+});
---
+++ pages/api/convert.ts
@@ -0,0 +1,120 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type ConvertionResult = {
+  isSuccess: boolean;
+  data: string;
+};
+
+export default (
+  req: NextApiRequest,
+  res: NextApiResponse<ConvertionResult>
+) => {
+  res.status(200).json({ isSuccess: true, data: 'Sixty two' });
+};
+
+export function convertToWords(numStr: string): ConvertionResult {
+  //Define number words constants
+  const MAX_ALLOWED_NUMBER = 9999999999999; //This number is kind of arbitrary
+  const NUMBER_ORDER = ['', '', 'thousand', 'million', 'billion', 'trillion'];
+  const NUMBER_ZERO = 'zero';
+  const NUMBER_FIRST_NAMED = [
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const NUMBER_TENS = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  //Validations
+  const num = +numStr;
+  if (isNaN(num) || !Number.isInteger(num) || num < 0) {
+    return {
+      isSuccess: false,
+      data: 'Number must be a positive integer',
+    };
+  }
+
+  if (num > MAX_ALLOWED_NUMBER) {
+    return {
+      isSuccess: false,
+      data: `Maximum allowed number is ${MAX_ALLOWED_NUMBER}`,
+    };
+  }
+
+  if (num === 0) {
+    return { isSuccess: true, data: NUMBER_ZERO };
+  }
+
+  const chunks = getChunksOfThree(numStr);
+  const order = chunks.length;
+  let result = '';
+
+  chunks.forEach((chunk, index) => {
+    if (+chunk) {
+      if (+chunk[0]) {
+        result += NUMBER_FIRST_NAMED[+chunk.charAt(0)] + ' hundred ';
+      }
+
+      if (+chunk[1] > 1) {
+        result += NUMBER_TENS[+chunk.charAt(1)];
+        if (+chunk.charAt(2)) {
+          result += '-' + NUMBER_FIRST_NAMED[+chunk.charAt(2)] + ' ';
+        }
+      } else {
+        result += NUMBER_FIRST_NAMED[+chunk.substring(1)] + ' ';
+      }
+
+      result += NUMBER_ORDER[order - index] + ' ';
+    }
+  });
+
+  result = result.trim();
+
+  return { isSuccess: true, data: result };
+}
+
+export function getChunksOfThree(n: string): Array<string> {
+  const totalChunks = Math.ceil(n.length / 3);
+  const remainder = n.length % 3;
+  const nWholeChuncks = n.length / 3;
+  const chunks = new Array<string>(totalChunks);
+
+  for (let i = 1; i <= nWholeChuncks; i++) {
+    const index = n.length - i * 3;
+    chunks[totalChunks - i] = n.substring(index, index + 3);
+  }
+
+  if (remainder) {
+    chunks[0] = n.substring(0, remainder).padStart(3, '0');
+  }
+
+  return chunks;
+}
