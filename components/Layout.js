ded465d4ad791ec0aac6808fc62c4c86622968dc
Eduardo Sacco <saccoeduardo@gmail.com>
2022-10-26 20:04:39 -0300

    Fix layout to position footer correctly depending on content

--- components/Layout.js
+++ components/Layout.js
@@ -4,19 +4,26 @@ import Footer from './Footer';
 import MenuAppBar from './MenuAppBar';
 
 export default function Layout(props) {
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
-                marginTop: '0',
-                marginBottom: 'auto',
-            }}
-        >
-            <MenuAppBar />
-            <main>{props.children}</main>
-            <Footer />
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        marginTop: '0',
+        marginBottom: 'auto',
+      }}
+    >
+      <MenuAppBar />
+      <Box
+        display="flex"
+        flexDirection="column"
+        padding={1}
+        marginBottom="auto"
+      >
+        {props.children}
+      </Box>
+      <Footer />
+    </Box>
+  );
 }
