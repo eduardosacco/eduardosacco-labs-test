8f82a43591d2bddc21ca2d2d13fafab5f1842476
Eduardo Sacco <saccoeduardo@gmail.com>
2022-10-26 15:44:21 -0300

    Add test and crypto links to menu

--- components/PositionedMenu.js
+++ components/PositionedMenu.js
@@ -5,48 +5,51 @@ import MenuItem from '@mui/material/MenuItem';
 import Link from '../src/Link';
 
 export default function PositionedMenu() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    return (
-        <div>
-            <Button
-                id="demo-positioned-button"
-                aria-controls={open ? 'demo-positioned-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}
-                sx={{ mr: 2 }}
-            >
-                MENU
-            </Button>
-            <Menu
-                id="demo-positioned-menu"
-                aria-labelledby="demo-positioned-button"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                }}
-            >
-                <Link href="/" color="secondary">
-                    <MenuItem onClick={handleClose}>Home</MenuItem>
-                </Link>
-                <MenuItem onClick={handleClose}>Numbers to words</MenuItem>
-                <MenuItem onClick={handleClose}>Logout</MenuItem>
-            </Menu>
-        </div>
-    );
+  return (
+    <div>
+      <Button
+        id="demo-positioned-button"
+        aria-controls={open ? 'demo-positioned-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        sx={{ mr: 2 }}
+      >
+        MENU
+      </Button>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <Link href="/" color="secondary">
+          <MenuItem onClick={handleClose}>Home</MenuItem>
+        </Link>
+        <Link href="/test" color="secondary">
+          <MenuItem onClick={handleClose}>Test</MenuItem>
+        </Link>
+        <Link href="/crypto" color="secondary"></Link>
+        <MenuItem onClick={handleClose}>Crypto</MenuItem>
+      </Menu>
+    </div>
+  );
 }
