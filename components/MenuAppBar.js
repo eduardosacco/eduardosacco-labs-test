7d69397f279c19bdb980fadae1de291719896d28
Eduardo Sacco <saccoeduardo@gmail.com>
2022-10-28 18:27:37 -0300

    Rename menu, format menu app bar

--- components/MenuAppBar.js
+++ components/MenuAppBar.js
@@ -5,20 +5,20 @@ import Typography from '@mui/material/Typography';
 import { Avatar } from '@mui/material';
 import ed from '../images/ed.gif';
 import Image from 'next/image';
-import PositionedMenu from './PositionedMenu';
+import PositionedMenu from './Menu';
 
 export default function MenuAppBar() {
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <PositionedMenu />
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    {`Ed's Experiments Lab`}
-                </Typography>
-                <Avatar alt="Eduardo Sacco">
-                    <Image src={ed} alt="Eduardo Sacco" layout="fill" />
-                </Avatar>
-            </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <PositionedMenu />
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {`Ed's Experiments Lab`}
+        </Typography>
+        <Avatar alt="Eduardo Sacco">
+          <Image src={ed} alt="Eduardo Sacco" layout="fill" />
+        </Avatar>
+      </Toolbar>
+    </AppBar>
+  );
 }
