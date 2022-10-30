8b26e573d64ed4c930349d0210dea8cd3353f8fc
Eduardo Sacco <saccoeduardo@gmail.com>
2022-10-30 02:03:57 -0300

    Update index context

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,21 @@ import Typography from '@mui/material/Typography';
 import Album from '../components/Album';
 import CardLongResponsive from '../components/CardLongResponsive';
 
+const projectContent = [
+  {
+    id: 1,
+    imgPath: '/images/d1.jpeg',
+    heading: 'Sweet Potato',
+    description: 'Some description',
+  },
+  {
+    id: 2,
+    imgPath: '/images/d2.jpeg',
+    heading: 'Sweet Potato',
+    description: 'Some description',
+  },
+];
+
 export default function Home() {
   return (
     <>
@@ -33,9 +48,12 @@ export default function Home() {
                 imgRight={true}
                 imgPath="/images/ed-banner.png"
                 imgAlt="ed banner"
-                heading="Hi i'm Ed!"
-                description="dasdsadasad dsadsada"
-                link="ads"
+                heading="Hi I'm Ed!"
+                description={`I am an electronic/software engineer from Argentina.
+                  I love to code and everything tech related. I am currently looking for my next place to continue my Web3 journey!
+                  I love going on mountaineering expeditions and going to the beach with my girlfriend and my dogs Nasta and Pantu.`}
+                linkText='Checkout my portfolio page'
+                link="https://eduardosacco.github.io/"
               />
             </Box>
           </Container>
@@ -65,7 +83,7 @@ export default function Home() {
                 dogs because I think they are cool.
               </Typography>
             </Box>
-            <Album />
+            <Album cards={projectContent} />
           </Box>
 
           <Box sx={{ my: 4 }}>
@@ -89,7 +107,9 @@ export default function Home() {
               imgPath="/images/electronic-duo.png"
               imgAlt="electronic duo"
               heading="This is my bro Gonza"
-              description="dasdsadasad dsadsada"
+              description={`We have been friends for more than 10 years! We have shared a lot through time.
+                We have done multiple mountaineering expeditions and there will be more in the future.
+                Together we are THE ELECTRONIC DUO.`}
             />
           </Container>
         </Box>
